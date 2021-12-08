9d14bf0d25490ee53045bd9ec3cc3f4bf23037dc
Renata Bezerra <83092450+renatabezerratech@users.noreply.github.com>
2021-12-08 15:56:06 -0300

    Update JS
    Criei um js para mudar título e fazê-lo interagir.

---
+++ Site Internet Segura/alterar.js
@@ -0,0 +1,7 @@
+//alteração de título
+var titulo = document.getElementById("tituloSegPag");
+titulo.innerHTML = "&#x1F47B;\nO que pode acontecer quando estamos navegando na internet?";
+//cria interação ao clicar no título
+var titulo = document.getElementById("tituloSegPag")
+titulo.onclick = function(){   titulo.innerHTML = "Podem tentar nos enganar!"};
+
